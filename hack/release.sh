3f1b2d62276232589c1818b70f43c78bbaf446ff
Chris Suszyński <ksuszyns@redhat.com>
2024-04-22 19:34:19 +0200

    Updateting the copy

--- hack/release.sh
+++ hack/release.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
+#
+# Copyright 2024 The Knative Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 set -Eeuo pipefail
 
 # Coordinates in GitHub.
 ORG_NAME="${ORG_NAME:-cardil}"
 
+# shellcheck disable=SC1090
 source "$(go run knative.dev/hack/cmd/script release.sh)"
 
 build_dir="${BUILD_DIR:-${REPO_ROOT_DIR}/build/_output}"
